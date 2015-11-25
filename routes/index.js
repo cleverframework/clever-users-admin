71d1f620c52a12bfa9f51be9ab5d1abddded1d62
JacopoDaeli <jacopo.daeli@gmail.com>
2015-11-25 09:39:59 +0000

    Add custom JWT_COOKIE_NAME

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,13 @@
 // Packages dependencies
 const router = require('express').Router()
 
-// JWT_COOKIE_NAME
-const JWT_COOKIE_NAME = 'cleverv1.jwt'
-
 // Exports
 module.exports = function (UsersAdminPackage, app, config) {
-  router.get('/login', (req, res, next) => res.send(UsersAdminPackage.render('auth')))
-  router.get('/signup', (req, res, next) => res.send(UsersAdminPackage.render('auth')))
+  const jwtCookieName = config.auth.jwt.cookie
+  router.get('/login', (req, res, next) => res.send(UsersAdminPackage.render('auth', { jwtCookieName })))
+  router.get('/signup', (req, res, next) => res.send(UsersAdminPackage.render('auth', { jwtCookieName })))
   router.get('/logout', (req, res, next) => {
-    res.clearCookie(JWT_COOKIE_NAME, { path: '/' })
+    res.clearCookie(jwtCookieName, { path: '/' })
     res.redirect('/login')
   })
   return router
